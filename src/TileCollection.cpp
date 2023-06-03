0b8ae2ade9e54e389d6043a2ad8c34ebd575fab4
extracheesypizza <FlowieYT@yandex.ru>
2023-06-04 01:53:26 +0300

    Look mom im smart

--- src/TileCollection.cpp
+++ src/TileCollection.cpp
@@ -46,115 +46,101 @@ void TileCollection::setDefinedTiles()
 
     // tile 1 [church]
     textures_[i]->loadFromFile("circuit/1_img.png");
-    current.setTexture(textures_[i]);
+    current.setTexture(textures_[i++]);
     current.setSockets({"DLD", "DTD", "DLD", "DTD"});
     possibleTiles_.push_back(current);
     // generateAllPossibleTiles();
-    i++;
 
     // tile 2 [3/4 city]
     textures_[i]->loadFromFile("circuit/2_img.png");
-    current.setTexture(textures_[i]);
+    current.setTexture(textures_[i++]);
     current.setSockets({"BIB", "BIB", "BIB", "BIB"});
     possibleTiles_.push_back(current);
     generateAllPossibleTiles();
-    i++;
 
     // tile 3 [3/4 city w/ road]
     textures_[i]->loadFromFile("circuit/3_img.png");
-    current.setTexture(textures_[i]);
+    current.setTexture(textures_[i++]);
     current.setSockets({"DLD", "DDB", "BIB", "BDD"});
     possibleTiles_.push_back(current);
     generateAllPossibleTiles();
-    i++;
 
     // tile 4 [1/2 city]
     textures_[i]->loadFromFile("circuit/4_img.png");
-    current.setTexture(textures_[i]);
+    current.setTexture(textures_[i++]);
     current.setSockets({"DDD", "DDD", "DDB", "BDD"});
     possibleTiles_.push_back(current);
     generateAllPossibleTiles();
-    i++;
 
     // tile 5 [1/2 city w/ road]
     textures_[i]->loadFromFile("circuit/5_img.png");
-    current.setTexture(textures_[i]);
+    current.setTexture(textures_[i++]);
     current.setSockets({"DLD", "DLD", "DLD", "DLD"});
     possibleTiles_.push_back(current);
     generateAllPossibleTiles();
-    i++;
 
     // tile 6 [city neck]
     textures_[i]->loadFromFile("circuit/6_img.png");
-    current.setTexture(textures_[i]);
+    current.setTexture(textures_[i++]);
     current.setSockets({"DLD", "DLD", "DDD", "DDD"});
     possibleTiles_.push_back(current);
     generateAllPossibleTiles();
-    i++;
 
     // tile 7 [2 * 1/4 city]
     textures_[i]->loadFromFile("circuit/7_img.png");
-    current.setTexture(textures_[i]);
+    current.setTexture(textures_[i++]);
     current.setSockets({"DDD", "DDD", "DDD", "DDD"});
     possibleTiles_.push_back(current);
     // generateAllPossibleTiles();
-    i++;
 
     // tile 8 [2 * 1/4 city v2]
     textures_[i]->loadFromFile("circuit/8_img.png");
-    current.setTexture(textures_[i]);
+    current.setTexture(textures_[i++]);
     current.setSockets({"DDD", "DLD", "DLD", "DLD"});
     possibleTiles_.push_back(current);
     generateAllPossibleTiles();
-    i++;
 
     // tile 9 [1/4 city w/ roads]
     textures_[i]->loadFromFile("circuit/9_img.png");
-    current.setTexture(textures_[i]);
+    current.setTexture(textures_[i++]);
     current.setSockets({"DLD", "DDD", "DLD", "DDD"});
     possibleTiles_.push_back(current);
     generateAllPossibleTiles();
-    i++;
 
     // tile 10 [2 roads]
     textures_[i]->loadFromFile("circuit/10_img.png");
-    current.setTexture(textures_[i]);
+    current.setTexture(textures_[i++]);
     current.setSockets({"DTD", "DDD", "DLD", "DDD"});
     possibleTiles_.push_back(current);
     generateAllPossibleTiles();
-    i++;
 
     // tile 11 [1 road]
     textures_[i]->loadFromFile("circuit/11_img.png");
-    current.setTexture(textures_[i]);
+    current.setTexture(textures_[i++]);
     current.setSockets({"DLD", "DLD", "DDD", "DDD"});
     possibleTiles_.push_back(current);
     generateAllPossibleTiles();
-    i++;
 
     // tile 12 [1 road]
     textures_[i]->loadFromFile("circuit/12_img.png");
-    current.setTexture(textures_[i]);
+    current.setTexture(textures_[i++]);
     current.setSockets({"DDD", "DLD", "DDD", "DLD"});
     possibleTiles_.push_back(current);
     generateAllPossibleTiles();
-    i++;
 
     // tile 12 [1 road]
     textures_[i]->loadFromFile("circuit/13_img.png");
-    current.setTexture(textures_[i]);
+    current.setTexture(textures_[i++]);
     current.setSockets({"DLD", "DDD", "DDD", "DDD"});
     possibleTiles_.push_back(current);
     generateAllPossibleTiles();
-    i++;
 
     // tile 12 [1 road]
     textures_[i]->loadFromFile("circuit/14_img.png");
-    current.setTexture(textures_[i]);
+    current.setTexture(textures_[i++]);
     current.setSockets({"DDD", "DTD", "DDD", "DTD"});
     possibleTiles_.push_back(current);
     generateAllPossibleTiles();
-    i++;
 }
 
 void TileCollection::outputAllPossibleTiles()
